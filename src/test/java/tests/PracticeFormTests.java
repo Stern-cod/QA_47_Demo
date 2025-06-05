afd5b8c9f02d2fc0610780d4745637cf0f64a5be
MedSveta <sveta1978medved@gmail.com>
2025-06-05 20:49:28 +0300

    Type Gender and DateOfBirth

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import dto.Student;
+import enums.Gender;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
@@ -9,8 +11,12 @@ import pages.PracticeFormPage;
 public class PracticeFormTests extends AppManager {
     @Test
     public void studentRegFormPositiveTest() {
+        Student student = new Student("Vasya", "Vasilev", "vasyavasilev@gmail.com",
+                Gender.MALE, "0123456789", "23 Mar 2013",
+                "Maths,Physics,Chemistry", "Sport",
+                "", "avenue 12", "NCR", "Delhi");
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
-        new PracticeFormPage(getDriver()).typePracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
     }
 }
